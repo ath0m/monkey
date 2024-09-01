a7885e4a632cfbf317f1d037902c5ff4c7ca674e
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2024-09-01 23:36:59 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Go development environment for aarch64-darwin";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        go
+        gopls
+        gotools
+      ];
+
+      shellHook = ''
+        export GOPATH=$(pwd)/.gopath
+        export PATH=$GOPATH/bin:$PATH
+        echo "Go development environment for aarch64-darwin ready!"
+      '';
+    };
+  };
+}
