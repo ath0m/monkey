1b8c6a76eb27bf4ecee51cf2f20e1d03c716c64d
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2024-09-15 00:19:33 +0100

    Completed section 2.6

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "Go development environment for aarch64-darwin";
+  description = "Go development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
   let
-    system = "aarch64-darwin";
+    system = "aarch64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
