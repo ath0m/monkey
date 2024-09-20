bf00a6207a9c9d6519347de2d8a2eed578d6f2bd
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2024-09-20 23:05:08 +0100

    Completed section 5.4

---
+++ ast/modify.go
@@ -0,0 +1,66 @@
+package ast
+
+type ModifierFunc func(Node) Node
+
+func Modify(node Node, modifier ModifierFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i] = Modify(statement, modifier).(Statement)
+		}
+
+	case *ExpressionStatement:
+		node.Expression = Modify(node.Expression, modifier).(Expression)
+
+	case *InfixExpression:
+		node.Left = Modify(node.Left, modifier).(Expression)
+		node.Right = Modify(node.Right, modifier).(Expression)
+
+	case *PrefixExpression:
+		node.Right = Modify(node.Right, modifier).(Expression)
+
+	case *IndexExpression:
+		node.Left = Modify(node.Left, modifier).(Expression)
+		node.Index = Modify(node.Index, modifier).(Expression)
+
+	case *IfExpression:
+		node.Condition = Modify(node.Condition, modifier).(Expression)
+		node.Consequence = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+
+	case *BlockStatement:
+		for i, statement := range node.Statements {
+			node.Statements[i] = Modify(statement, modifier).(Statement)
+		}
+
+	case *ReturnStatement:
+		node.ReturnValue = Modify(node.ReturnValue, modifier).(Expression)
+
+	case *LetStatement:
+		node.Value = Modify(node.Value, modifier).(Expression)
+
+	case *FunctionLiteral:
+		for i := range node.Parameters {
+			node.Parameters[i] = Modify(node.Parameters[i], modifier).(*Identifier)
+		}
+		node.Body = Modify(node.Body, modifier).(*BlockStatement)
+
+	case *ArrayLiteral:
+		for i := range node.Elements {
+			node.Elements[i] = Modify(node.Elements[i], modifier).(Expression)
+		}
+
+	case *HashLiteral:
+		newPairs := make(map[Expression]Expression)
+		for key, val := range node.Pairs {
+			newKey := Modify(key, modifier).(Expression)
+			newVal := Modify(val, modifier).(Expression)
+			newPairs[newKey] = newVal
+		}
+		node.Pairs = newPairs
+
+	}
+	return modifier(node)
+}
--- evaluator/quote_unqoute_test.go
+++ evaluator/quote_unqoute_test.go
@@ -46,3 +46,70 @@ func TestQuote(t *testing.T) {
 		})
 	}
 }
+
+func TestQuoteUnquote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`quote(unquote(4))`,
+			`4`,
+		},
+		{
+			`quote(unquote(4 + 4))`,
+			`8`,
+		},
+		{
+			`quote(8 + unquote(4 + 4))`,
+			`(8 + 8)`,
+		},
+		{
+			`quote(unquote(4 + 4) + 8)`,
+			`(8 + 8)`,
+		},
+		{
+			`let foobar = 8;
+			quote(foobar)`,
+			`foobar`,
+		},
+		{
+			`let foobar = 8;
+			quote(unquote(foobar))`,
+			`8`,
+		},
+		{
+			`quote(unquote(true))`,
+			`true`,
+		},
+		{
+			`quote(unquote(true == false))`,
+			`false`,
+		},
+		{
+			`quote(unquote(quote(4 + 4)))`,
+			`(4 + 4)`,
+		},
+		{
+			`let quotedInfixExpression = quote(4 + 4);
+			quote(unquote(4 + 4) + unquote(quotedInfixExpression))`,
+			`(8 + (4 + 4))`,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			quote, ok := evaluated.(*object.Quote)
+			if !ok {
+				t.Fatalf("expected *object.Quote. got=%T (%+v)", evaluated, evaluated)
+			}
+			if quote.Node == nil {
+				t.Fatal("quote.Node is nil")
+			}
+			if quote.Node.String() != tt.expected {
+				t.Errorf("not equal. got=%q, want=%q", quote.Node.String(), tt.expected)
+			}
+		})
+	}
+}
